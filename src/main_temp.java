3dd35360f2d6053b6e6b357d376219226b911f4b
Brill Baker <103843778+LincolnK27@users.noreply.github.com>
2023-11-01 13:05:11 -0500

    Webhook Test
    Testing the Github Webhook, to seemlessly post about GitHub updates in discord.

--- src/main_temp.java
+++ src/main_temp.java
@@ -3,7 +3,7 @@ public class main_temp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// please work ~ LK 10/31/23
+		// delete later, this is only here for the GitHub Test ~ Lk
 	}
 
 }
