8f252ed8b3c531d2d42088ab327421bc6f46d66a
IWolski <105333264+IWolski@users.noreply.github.com>
2023-11-01 15:10:05 -0500

    Logging
    Basic Logging added from class examples

--- src/main_temp.java
+++ src/main_temp.java
@@ -1,9 +1,17 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class main_temp {
 
+	// Logging Object
+	final static Logger log = 
+			LogManager.getLogger(main_temp.class.getName());
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// please work ~ LK 10/31/23
+
+		log.debug("Hello World!");
+		log.info("Hello World, Again!");
+
 	}
 
 }
