3f31bc8d86cc345821447d3087a4d38ccaf77cd7
IWolski <105333264+IWolski@users.noreply.github.com>
2023-11-01 16:19:26 -0500

    Documentation
    Added more comments

--- src/main_temp.java
+++ src/main_temp.java
@@ -1,6 +1,9 @@
+//Import the necessary files for logging
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+//A temporary class used to practice and set up the logging
+//May be used for other testing in the future.
 public class main_temp {
 
 	// Logging Object
